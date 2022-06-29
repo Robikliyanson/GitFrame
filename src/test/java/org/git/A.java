9cbdcbcd8d36982e30b40a9bee257bea95992d3d
Robikliyanson <krishkrash@gmail.com>
2022-06-29 07:06:55 +0530

    GitFrameMain

---
+++ src/test/java/org/git/A.java
@@ -0,0 +1,26 @@
+package org.git;
+
+import org.testng.annotations.Test;
+
+public class A {
+	
+	
+	@Test
+	private void test() {
+		System.out.println("test");
+
+	}
+	
+	@Test
+	private void test1() {
+		System.out.println("test1");
+
+	}
+	
+	@Test
+	private void test2() {
+		System.out.println("test2");
+
+	}
+
+}
---
+++ src/test/java/org/git/GitFrameTest.java
@@ -0,0 +1,21 @@
+package org.git;
+
+import org.testng.annotations.Test;
+
+public class GitFrameTest {
+	
+	@Test
+	private void india() {
+		System.out.println("india");
+	}
+	@Test
+		private void india1() {
+			System.out.println("india1");
+	}
+	@Test
+			private void india2() {
+				System.out.println("india2");
+
+	}
+
+}
